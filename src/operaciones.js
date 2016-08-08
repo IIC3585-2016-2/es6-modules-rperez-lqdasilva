ecc922cc63ea1e2b225f44f22b5ca71f2da1f68b
Luiz Quelves <luiz.quelves@gmail.com>
2016-08-08 11:25:49 -0400

    Adicionando archivos de Ronald

---
+++ src/operaciones.js
@@ -0,0 +1,31 @@
+
+
+export var numeros = {  
+  numero1: 5,
+  numero2: 2,
+};
+
+export {resta,multiplicación};
+
+export {división as div};
+
+export default function sumar(numero1, numero2) {
+  
+  return numero1 + numero;
+}
+ 
+function resta(numero1, numero2) {
+  
+  return numero1 - numero;
+}
+ 
+function multiplicación(numero1, numero2) {
+  
+  return numero1 * numero;
+}
+
+function división (numero1,numero2){
+	return numero1/numero2;
+}
+
+
